e446f6ff4f6e0caed7a7a8799cc51f7ab1f56350
ViditKShah <vidit_kshah99@rocketmail.com>
2019-05-05 09:26:39 -0400

    Adding validation to the customer form

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,11 +10,12 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The Customer Name is required.")]
         [StringLength(255)]
         public string Name { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
