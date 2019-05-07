f8989fcb5f8f6d4eff9d324679f7736185da541b
ViditKShah <vidit_kshah99@rocketmail.com>
2019-05-06 21:23:34 -0400

    Feeding data into DT along with eager loading

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -1,8 +1,7 @@
 ﻿using AutoMapper;
 using System;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web.Http;
 using Vidly.Dtos;
 using Vidly.Models;
@@ -21,8 +20,10 @@ namespace Vidly.Controllers.Api
         // GET /api/customers
         public IHttpActionResult GetCustomers()
         {
-            var customerDtos = _context.Customers.ToList()
-                                        .Select(Mapper.Map<Customer, CustomerDto>);
+            var customerDtos = _context.Customers
+                                    .Include(c => c.MembershipType)
+                                    .ToList()
+                                    .Select(Mapper.Map<Customer, CustomerDto>);
             return Ok(customerDtos);
         }
 
